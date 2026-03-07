8f182c7f8a6250eae70c2e2a28ef12c974e5c204
WEareinthebeam <70709450+WEareinthebeam@users.noreply.github.com>
2026-03-07 20:34:44 +0100

    Added SceneCollectionManager
    + Added SceneCollectionManager - Used for nonDeferred/Deferred collecting PackedScenes with potential error logging
    = Replaced ResourceLoader.load() with SceneCollectionManager.get_packed_scene_deferred() in Main scene

--- assets/scripts/scene_scripts/main/main.gd
+++ assets/scripts/scene_scripts/main/main.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 
 func start_game(scene_to_remove : Node) -> void:
 	# Loading demo game scene
-	var scene:PackedScene = load("res://assets/scenes/demo_movement.tscn")
+	var scene:PackedScene = await SceneCollectionManager.get_packed_scene_deferred("res://assets/scenes/demo_movement.tscn")
 	# Instancing of the scene
 	var scene_instance = scene.instantiate()
 	# Adds scene_instance as child of the Main node
@@ -14,9 +14,10 @@ func start_game(scene_to_remove : Node) -> void:
 	if scene_to_remove != null :
 		scene_to_remove.queue_free()
 
+
 func to_main_menu() -> void:
 	# Loading scene
-	var scene:PackedScene = load("res://assets/scenes/main_menu.tscn")
+	var scene:PackedScene = await SceneCollectionManager.get_packed_scene_deferred("res://assets/scenes/main_menu.tscn")
 	# Instancing of the scene
 	var scene_instance = scene.instantiate()
 	# Adds main_menu as child of the Main node
